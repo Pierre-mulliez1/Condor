308b57942e902575c6165310c5310c57464dc535
Pierre Mulliez <37341575+Pierre-mulliez1@users.noreply.github.com>
2020-10-13 11:58:18 +0200

    index design

---
+++ test.js
@@ -0,0 +1,37 @@
+var counter =0 ;
+var counter1 = 0;
+function myFunctionsend() {
+    document.querySelector("#hub").innerHTML = document.querySelector("#hub").innerHTML.replace(document.querySelector("#message").innerHTML);
+  /// node_value method ?
+}
+
+function Functionexpand() {
+    if (counter == 0)
+    {
+        document.getElementById("sugg").style.gridTemplateAreas =  
+    "'avatar header header header header header header ' 'menu menu menu menu menu menu menu' 'left main main main right right right' 'footer footer footer footer footer footer footer'"; 
+    counter = 1;
+    }
+    else {
+        document.getElementById("sugg").style.gridTemplateAreas =  
+    "'avatar header header header header header header' 'menu menu menu menu menu menu menu' 'left main main main main main right' 'footer footer footer footer footer footer footer'"; 
+    counter = 0;
+    }
+   
+}
+   
+function Functionexpand2() {
+    if (counter1 == 0)
+    {
+        document.getElementById("sugg").style.gridTemplateAreas =  
+    "'avatar header header header header header header' 'menu menu menu menu menu menu menu' 'left left left main main main right' 'footer footer footer footer footer footer footer'"; 
+    counter1 = 1;
+    }
+    else {
+        document.getElementById("sugg").style.gridTemplateAreas =  
+    "'avatar header header header header header header' 'menu menu menu menu menu menu menu' 'left main main main main main right' 'footer footer footer footer footer footer footer'"; 
+    counter1 = 0;
+    }
+}
+
+
